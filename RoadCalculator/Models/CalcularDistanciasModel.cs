e3cfbe0d1cdaa0b87883db8ed7595b2288f33cd5
Nelson Marro <nelsonmarro99@gmail.com>
2023-07-16 19:57:03 -0500

    Add project files.

---
+++ RoadCalculator/Models/CalcularDistanciasModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoadCalculator.Models;
+
+public class CalcularDistanciasModel
+{
+    [Range(1, double.MaxValue, ErrorMessage = "El valor de {0} debe ser mayor a {1}")]
+    public double PrecioAlquilerMaquina { get; set; }
+}
---
+++ RoadCalculator/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace RoadCalculator.Models;
+
+public class Reuslt
+{
+    public class Result
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Distance { get; set; }
+        public double InstallationTime { get; set; }
+        public double Cost { get; set; }
+    }
+}
